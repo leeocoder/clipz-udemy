67291c15573e55e9566794817026609cc6d22672
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-02-20 06:32:38 -0300

    feat: add modal service

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent {
-
+  constructor(public modal: ModalService) {}
 }
