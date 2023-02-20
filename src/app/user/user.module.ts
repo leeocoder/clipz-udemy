d598d6135bdd5b230e9060376d13f88dc710b2b0
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-02-20 05:32:08 -0300

    chore: add modal component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthModalComponent
+  ],
   imports: [
     CommonModule
   ]
