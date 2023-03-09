a471723f9a725d6c11633763b20e688af127e0c7
Leonardo <leeodesign@hotmail.com>
2023-03-09 06:33:31 -0300

    validators

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,22 +2,28 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IUser } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private emailTaken: EmailTaken
+  ) {}
   inSubmission: boolean = false;
   name: FormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(3),
   ]);
-  email: FormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  email: FormControl = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age: FormControl = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -38,14 +44,17 @@ export class RegisterComponent {
   alertMessage: string = 'Please Wait! Your account is being created.';
   alertColor: string = 'blue';
 
-  registerForm: FormGroup = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirmPassword: this.confirmPassword,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirmPassword')]
+  );
 
   async register($event: Event): Promise<void> {
     this.inSubmission = true;
